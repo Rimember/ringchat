c221f3a38cd338850233565c3000108ae0cd542a
rudeuns <rudeuns@gmail.com>
2024-10-20 02:38:31 +0900

    test: Backend Green CI/CD test6

--- backend/app/llm/embedding.py
+++ backend/app/llm/embedding.py
@@ -1,4 +1,3 @@
-from sentence_transformers import SentenceTransformer
 from langchain_community.embeddings import OpenAIEmbeddings
 from typing import List
 
@@ -6,10 +5,8 @@ from typing import List
 def generate_embedding(text: str, model_type: str = "openai") -> List[float]:
     if model_type == "openai":
         return generate_openai_embedding(text)
-    elif model_type == "sbert":
-        return generate_sbert_embedding(text)
     else:
-        raise ValueError("Unsupported model_type. Choose 'openai' or 'sbert'.")
+        raise ValueError("Unsupported model_type.")
 
 
 def generate_openai_embedding(text: str) -> List[float]:
@@ -20,9 +17,9 @@ def generate_openai_embedding(text: str) -> List[float]:
     return embedding_vector
 
 
-def generate_sbert_embedding(text: str) -> List[float]:
-    model = SentenceTransformer(
-        "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-    )  # 384 dim
-    embedding_vector = model.encode(text).tolist()
-    return embedding_vector
+# def generate_sbert_embedding(text: str) -> List[float]:
+#     model = SentenceTransformer(
+#         "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+#     )  # 384 dim
+#     embedding_vector = model.encode(text).tolist()
+#     return embedding_vector
